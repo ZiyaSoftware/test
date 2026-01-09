2713753ccf5d870c75cb3d27292eac32719243c9
Ziya <ziyasoftware123@gmail.com>
2026-01-09 22:31:25 +0530

    add my code files

---
+++ owl/social.js
@@ -0,0 +1,17 @@
+$('#owl-carousel-3 .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+	dots:false,
+    nav:false,
+    responsive:{
+        0:{
+            items:5
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:5
+        }
+    }
+});
